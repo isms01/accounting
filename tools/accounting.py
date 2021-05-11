c12a58d49e3fa358e8630b79752f9d4fc90bc8b9
Masa <ishimasa01@gmail.com>
2021-05-11 09:58:02 +0900

    change item name, price and amount

--- tools/accounting.py
+++ tools/accounting.py
@@ -2,6 +2,6 @@ class Accounting:
     val = []
 
     def __init__(self):
+        self.val.append("coffee")
+        self.val.append(500)
         self.val.append(1)
-        self.val.append(2)
-        self.val.append(3)
