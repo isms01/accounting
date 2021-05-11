0cc635d9e50e7364a36df42015530e0879d1f6ee
Masa <ishimasa01@gmail.com>
2021-05-11 09:54:10 +0900

    rename and delete some files for tidying up.

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
 import argparse
 
-from tools import profit
-from tools import table
+from tools import accounting
+
 
 def main(args):
-    input = args.input
+    # input = args.input
+    item1 = accounting.Accounting()
+    print(item1.val)
 
-    record = table.Record()
-    record.show()
-    # record.register()
-    record.show()
-    
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input', type=str)
+    # parser.add_argument("--input", type=str)
     args = parser.parse_args()
     main(args)
---
+++ tools/accounting.py
@@ -0,0 +1,7 @@
+class Accounting:
+    val = []
+
+    def __init__(self):
+        self.val.append(1)
+        self.val.append(2)
+        self.val.append(3)
